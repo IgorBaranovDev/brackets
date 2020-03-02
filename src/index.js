330c8163b47af07b2aabdd8f2044aba3c06a24ef
Baranov Igor <igor.82bv@mail.ru>
2020-03-02 23:46:58 +0300

    feat:final  solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let pairArray = [];
+    let strLength = str.length;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        pairArray.push(bracketsConfig[i].join(""));
+    }
+    for (let j = 0; j < strLength; j++) {
+        for (let k = 0; k < pairArray.length; k++) {
+            if (str.includes(pairArray[k])) {
+                str = str.replace(pairArray[k], "");
+            }
+        }
+    }
+    return str.length == 0 ? true : false;
+};
